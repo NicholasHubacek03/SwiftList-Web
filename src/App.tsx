bc1efe159332fb9fbe4d399175a09d91b1c0aaf9
Nicholas <Nicholas.Hubacek@gmail.com>
2024-01-16 14:00:13 -0600

    added login folder and propiters

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -6,28 +8,26 @@ function App() {
 
   return (
     <>
-    <form>
-      <h1>SwiftList</h1>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
       <div className="card">
-      
-      <label htmlFor="username">Email:</label>
-        <input
-          type="text"
-          id="username"
-        />
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-        />
-      </div>
-      <button>Login</button>
-      <div>
-        <button>don't have an account? Sign up!</button>
-      </div>
-      </form>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
