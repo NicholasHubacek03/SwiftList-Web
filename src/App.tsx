2b38d42dc1a5c847d3dc9a6f61a74ee5c096aad7
Nicholas <Nicholas.Hubacek@gmail.com>
2024-01-17 17:01:23 -0600

    fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import './App.css'
-import { Route, BrowserRouter as Router } from 'react-router-dom'
-import MainComponent from './components/MainComponent'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Login from './containers/Login/Login'
+import React from 'react';
+import { AboutUs, Home } from './components/Pages';
 
 
 
-  const App: React.FC = () => {
+
+class App extends React.Component {
+  render() {
     return (
-      <Route path="/" element={<Login />} />
-    )
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">HomePage</Link>
+            <Link to="/Login">Login</Link>
+            <Link to="/AboutUs">AboutUs</Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/AboutUs" element={<AboutUs />} />
+          </Routes>
+        </div>
+      </Router>
+    );
   }
-
-
+}
 
 
 export default App
---
+++ src/components/Pages.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Home = () => <h1>Home Page</h1>;
+export const Login = () => <h1>Foo Page</h1>;
+export const AboutUs = () => <h1>Bar Page</h1>;
